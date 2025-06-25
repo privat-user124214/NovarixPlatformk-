56cab64ddccdd51fb22860e9a246a67067ce2896
user-z55554dfgfh <novarix.studio@gmail.com>
2025-06-25 13:06:38 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // wichtig für Render-Hosting
+  server: {
+    port: 3000, // z. B. hier Port 3000 statt 5173
+  },
 });
