d676c291fecb0c2b8a01b3c78b60efb0c970b450
user-z55554dfgfh <novarix.studio@gmail.com>
2025-06-25 11:04:41 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ weil du eine eigene Domain nutzt
   plugins: [react()],
-});
+  base: '/', // oder einfach weglassen
+})
