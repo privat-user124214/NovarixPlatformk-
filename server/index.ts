2b67bb82bc3f47067c8feb3a89ea2f676a021537
user-z55554dfgfh <novarix.studio@gmail.com>
2025-06-25 14:59:07 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
+import session from "express-session";
 import cookieParser from "cookie-parser";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
