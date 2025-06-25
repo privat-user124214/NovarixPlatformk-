7ece046f1119ea3d1267b5d9159d0f97715319fb
user-z55554dfgfh <novarix.studio@gmail.com>
2025-06-25 07:02:26 +0000

    Initial commit

--- upload-to-github.sh
+++ upload-to-github.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+# Dieses Skript fügt das GitHub-Repository hinzu und pusht das Projekt
+
+# Remote-Repository hinzufügen
 git remote add origin git@github.com:privat-user124214/downloade.git
+
+# Branch umbenennen, falls nötig
 git branch -M main
+
+# Weitere Dateien zum Staging hinzufügen
+git add .
+
+# Commit erstellen
+git commit -m "Initial commit"
+
+# Hochladen des Codes (hier wirst du nach deinem Benutzernamen und deinem Token gefragt)
 git push -u origin main
